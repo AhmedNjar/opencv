5564272de0ef2710c8d29b3931b8c25ce99edb7c
dadas566464 <supigner@gmail.com>
2026-05-27 17:48:40 +0300

    Add adaptive and Otsu's thresholding to image processing

--- thresh.py
+++ thresh.py
@@ -32,5 +32,14 @@ thresh_adapt = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THR
 # 11: The block size to be used for the adaptive thresholding. This is the size of the neighborhood area that is used to calculate the adaptive threshold value for each pixel. It should be an odd number (e.g., 3, 5, 7, etc.).
 # 3: The constant (C) to be subtracted from the mean or weighted mean calculated for the adaptive thresholding. This constant is used to fine-tune the thresholding result. A positive value will decrease the threshold value, while a negative value will increase it. In this case, we are using a constant of 3, which means that the adaptive threshold value will be the mean of the neighborhood area minus 3.    
 cv.imshow('Adaptive Threshold', thresh_adapt)
+# You can inverse the adaptive thresholding result by using cv.THRESH_BINARY_INV instead of cv.THRESH_BINARY in the cv.adaptiveThreshold function.
+
+# Otsu's thresholding
+_, thresh_otsu = cv.threshold(gray, 125, 255, cv.THRESH_BINARY + cv.THRESH_OTSU) # The cv.threshold function is used to apply Otsu's thresholding operation to the grayscale image. The parameters are as follows:
+# [gray]: The source image for which the thresholding operation is to be applied. It should be a single-channel image (grayscale).
+# 125: The threshold value. This value is ignored when using Ots u's thresholding, as the optimal threshold value is automatically determined by the algorithm.
+# 255: The maximum value to use with the Otsu's thresholding. This is the value that is assigned to the pixels that are classified as foreground (greater than the optimal threshold value).
+# cv.THRESH_BINARY + cv.THRESH_OTSU: The type of thresholding to be applied. In this case, we are using a combination of the THRESH_BINARY type and the THRESH_OTSU flag. The THRESH_OTSU flag tells the function to automatically determine the optimal threshold value using Otsu's method, which minimizes the intra-class variance of the pixel values.    
+cv.imshow('Otsu Threshold', thresh_otsu)    
 
 cv.waitKey(0)
